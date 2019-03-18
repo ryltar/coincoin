98434a6bfa983bbd3d68de26ef207e09d963f02d
Guillaume Martin <mart1.guillaume@free.fr>
2019-03-18 18:15:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,12 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include "coincoinminer.cpp"
-
-
-using namespace std;
-
-inline bool checkArgs(int nbr) {
-    try {
-        if (nbr < 2 || nbr > 6) {
-            throw string("Probleme d'arguments !");
-        }
-    } catch (string const& chaine) {
-        cerr << chaine << "\n";
-        return false;
-    }
-    return true;
-}
-
-int main( int argc, char *argv[], char *envp[] ) {
-
-    bool isCheck = checkArgs(argc);
-    if (!isCheck) {
-        return 1;
-    }
-
-    string param1(argv[1]);
+int main(int argc, char** argv) {
+    srand(time(0));
     auto *miner = new CoinCoinMiner();
-    if (param1 == "-z") {
-        miner->benchmark();
-        if (argc > 2) {
-            string param2(argv[2]);
-            string tri(argv[3]);
-            string param4(argv[4]);
-            string min(argv[5]);
-            int minInt = stoi(min);
-            miner->mining(tri,minInt);
-        }
-    } else if (param1 == "-tri") {
-        string tri(argv[2]);
-        string param4(argv[3]);
-        string min(argv[4]);
-        int minInt = stoi(min);
-        miner->mining(tri, minInt);
+    bool commandLineTrue = miner->checkArguments(argc);
+    if (!commandLineTrue) {
+        return 1;
+    }else{
+        miner->chooseOptions(argv);
     }
+
+    return 0;
 }
